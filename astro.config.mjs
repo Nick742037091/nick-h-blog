8683adfe5b188ce0d8cd79d1adb72cd0e9782331
Nick742037091 <742037091@qq.com>
2025-06-04 10:11:45 +0800

    ✨ feat: 调整项目baseUrl

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
   site: "https://nick-h.netlify.app/",
-  base: "/",
+  base: "/blog",
   trailingSlash: "always",
   integrations: [
     tailwind(
